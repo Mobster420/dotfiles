cc5f1f617031148e9a817687563fa48b33e9cf14
M0ba <m0bahosting@gmail.com>
2021-04-07 13:06:20 +0200

    Brightness and permissions update

--- install.sh
+++ install.sh
@@ -14,9 +14,13 @@ sudo pacman -S dunst picom i3 rofi
 yay -S polybar 
 
 # Setting up the dotfiles
-
 git clone https://github.com/Mobster420/dotfiles
 cd dotfiles
 cp * ~/.config/
+
+# Setting the permission for the bash files
+chmod +x ~/.config/polybar/launch.sh
+chmod +x ~/.config/scripts/brightness2.sh
+chmod +x ~/.config/scripts/brightness.sh
 
 echo "Setup is done!"
